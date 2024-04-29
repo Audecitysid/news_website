993650848a5a08294a0584e9e1282ce247658646
Siddhesh Dhomane <audecitysid@gmail.com>
2024-04-29 08:12:59 +0530

    serverless function update

--- netlify/functions/get_content.js
+++ netlify/functions/get_content.js
@@ -3,10 +3,12 @@ import fetch from 'node-fetch';
 
 exports.handler = async function(event, context) {
     const API_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5ff6da31ba214f1285962596a3670a8b";
+    console.log("api request made");
 
     try {
         const response = await fetch(API_URL);
         const data = await response.json();
+        console.log(data);
         return {
             statusCode: 200,
             body: JSON.stringify(data)
