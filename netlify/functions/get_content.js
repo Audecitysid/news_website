6139dbd34237b5dcdfe9991dcc1cd890587c338f
Siddhesh Dhomane <audecitysid@gmail.com>
2024-04-29 08:19:11 +0530

    serverless function update

--- netlify/functions/get_content.js
+++ netlify/functions/get_content.js
@@ -1,22 +1,24 @@
-// Use dynamic import for fetch instead of require
-import fetch from 'node-fetch';
-
-exports.handler = async function(event, context) {
-    const API_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5ff6da31ba214f1285962596a3670a8b";
-    console.log("api request made");
-
-    try {
-        const response = await fetch(API_URL);
-        const data = await response.json();
-        console.log(data);
-        return {
-            statusCode: 200,
-            body: JSON.stringify(data)
-        };
-    } catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify({ msg: error.message })
-        };
-    }
-};
+// Asynchronous import for node-fetch
+import('node-fetch').then(({ default: fetch }) => {
+    exports.handler = async function(event, context) {
+        const API_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5ff6da31ba214f1285962596a3670a8b";
+        console.log("api request made");
+  
+        try {
+            const response = await fetch(API_URL);
+            const data = await response.json();
+            console.log(data);
+            return {
+                statusCode: 200,
+                body: JSON.stringify(data)
+            };
+        } catch (error) {
+            console.error("Fetch error: ", error);
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ msg: error.message })
+            };
+        }
+    };
+  }).catch(error => console.error("Failed to load 'node-fetch':", error));
+  
