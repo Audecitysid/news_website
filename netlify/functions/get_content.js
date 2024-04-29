55c4b60443618caec72d7ab1ceae8da6b8100e1d
Siddhesh Dhomane <audecitysid@gmail.com>
2024-04-29 08:34:27 +0530

    serverless function update

--- netlify/functions/get_content.js
+++ netlify/functions/get_content.js
@@ -1,35 +1,19 @@
-// Top-level asynchronous import of node-fetch
-let fetch;
+// Import axios directly
+const axios = require('axios');
 
-import('node-fetch')
-  .then(({ default: importedFetch }) => {
-    fetch = importedFetch;
-  })
-  .catch(error => console.error("Failed to load 'node-fetch':", error));
-
-// Exporting handler function directly under exports
 exports.handler = async function(event, context) {
-  if (!fetch) {
-    console.error("Fetch is not loaded");
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ msg: "Fetch is not initialized" })
-    };
-  }
-
   const API_URL = "https://newsapi.org/v2/top-headlines?country=us&apiKey=5ff6da31ba214f1285962596a3670a8b";
   console.log("API request made");
 
   try {
-    const response = await fetch(API_URL);
-    const data = await response.json();
-    console.log(data);
+    const response = await axios.get(API_URL);
+    console.log(response.data);
     return {
       statusCode: 200,
-      body: JSON.stringify(data)
+      body: JSON.stringify(response.data)
     };
   } catch (error) {
-    console.error("Fetch error: ", error);
+    console.error("API request error: ", error);
     return {
       statusCode: 500,
       body: JSON.stringify({ msg: error.message })
